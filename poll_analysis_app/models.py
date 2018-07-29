e5295e7aab2c57d9df86347e602fb5c44b77cfc1
Marcel <heinz@uni-koblenz.de>
2018-07-29 03:35:50 +0200

    plotting with django-pandas in the Django tutorial

--- poll_analysis_app/models.py
+++ poll_analysis_app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.utils import timezone
 import datetime
-
+from django_pandas.io import read_frame
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,7 +14,29 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def results_table(self):
+        df = read_frame(self.choice_set.all(), fieldnames=[         'choice_text', 'votes'])
+        return df.to_html()
+
+    def results_plot(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        df = read_frame(self.choice_set.all())
+        df.plot(x='choice_text', y='votes', kind='bar', ax=ax)
+        
+        #save figure as bytes
+        io = BytesIO() 
+        fig.savefig(io, format='png')
+        
+        #decode bytes back to String
+        data = base64.encodebytes(io.getvalue()).decode('UTF-8')
+        
+        #fill data into html-img
+        html = '<img src="data:image/png;base64,{}" width="50%"/>'
+        return html.format(data)
 
 
 class Choice(models.Model):
@@ -20,5 +45,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.question_text
+        return self.choice_text
 
